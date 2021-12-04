eb80cbb8cbc9a60faea1c055152376227bb85625
JordanAlexanderIT <jordan@alexanderit.org>
2021-12-04 21:41:19 +0200

    Added morgan
    Morgan added.

--- src/Components/nodeapp.js
+++ src/Components/nodeapp.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const nodeapp = express();
+const morgan = require("morgan"); // Morgan is for error-handling
 
 const productRoutes = require("../api/routes/products");
 const orderRoutes = require("../api/routes/orders");
 
 nodeapp.use("/products", productRoutes);
-
 nodeapp.use("/orders", orderRoutes);
 
 module.exports = nodeapp;
