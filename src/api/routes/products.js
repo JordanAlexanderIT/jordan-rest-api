de85f54d843d53ee895ce0a2bdf1f8f8ccddfe16
JordanAlexanderIT <88573032+JordanAlexanderIT@users.noreply.github.com>
2021-12-12 13:49:51 +0200

    JWT Coming along well

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
@@ -61,7 +63,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", upload.single("productImage"), (req, res, next) => {
+router.post("/", checkAuth, upload.single("productImage"), (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -40,6 +41,23 @@ router.post("/login", (req, res, next) => {
         if (err) {
           return res.status(401).json({ message: "Auth failed." });
         }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0]["email"],
+              userId: user[0]["_id"],
+            },
+            "secret",
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        return res.status(401).json({ message: "Auth failed." });
       });
     })
     .catch((err) => {
