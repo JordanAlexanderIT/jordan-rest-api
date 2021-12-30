2ed6afed5e9a5856d83cd4483f8ae8e945918914
JordanAlexanderIT <88573032+JordanAlexanderIT@users.noreply.github.com>
2021-12-30 14:30:11 +0200

    X
    Some changes

--- src/api/middleware/check-auth.js
+++ src/api/middleware/check-auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     console.log(token);
-    const decoded = jwt.verify(token, "secret");
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
     req.userData = decoded;
     next();
   } catch (error) {
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const User = require("../models/user");
 
@@ -47,7 +48,7 @@ router.post("/login", (req, res, next) => {
               email: user[0]["email"],
               userId: user[0]["_id"],
             },
-            "secret",
+            JWT_SECRET_KEY,
             {
               expiresIn: "1h",
             }
